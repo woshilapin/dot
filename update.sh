0fd503d5a3de6f2782ec8abd58832a1c19eeb58e
Jean SIMARD <woshilapin@tuziwo.info>
2016-08-24 23:18:16 -0400

    [dot] Clean and improve the quality of script 'update.sh'

--- update.sh
+++ update.sh
@@ -2,61 +2,65 @@
 
 print_msg()
 {
-	echo "dot-files: "$1
+	echo "dot-files: ${1}"
 }
 
 dot_file()
 {
-	if test ! -L ~/.$2
+	ROOT_DIR="${1}"
+	FILE="${2}"
+	if test ! -L "${HOME}/.${FILE}"
 	then
-		if test -f ~/.$2
+		if test -f "${HOME}/.${FILE}"
 		then
-			print_msg "WARNING: File ~/.$2 already exists"
+			print_msg "WARNING: File ${HOME}/.${FILE} already exists"
 			DATE=$( date +'%Y%m%d-%H%M%S' )
-			print_msg "INFO: File ~/.$2 backup to ~/.$2.${DATE}"
-			mv ~/.$2 ~/.$2.${DATE}
+			print_msg "INFO: File ${HOME}/.${FILE} backup to ${HOME}/.${FILE}.${DATE}"
+			mv "${HOME}/.${FILE}" "${HOME}/.${FILE}.${DATE}"
 		fi
-		print_msg "UPDATE: Create link ~/.$2 to $1/$2"
-		ln -s $1/$2 ~/.$2
+		print_msg "UPDATE: Create link ${HOME}/.${FILE} to ${ROOT_DIR}/${FILE}"
+		ln -s "${ROOT_DIR}/${FILE}" "${HOME}/.${FILE}"
 	fi
 	return 0
 }
 
 dot_dir()
 {
-	if test ! -L ~/.$2
+	ROOT_DIR="${1}"
+	DIR="${2}"
+	if test ! -L "${HOME}/.${DIR}"
 	then
-		if test -f ~/.$2
+		if test -d "${HOME}/.${DIR}"
 		then
-			print_msg "WARNING: Directory ~/.$2 already exists"
+			print_msg "WARNING: Directory ${HOME}/.${DIR} already exists"
 			DATE=$( date +'%Y%m%d-%H%M%S' )
-			print_msg "INFO: Directory ~/.$2 backup to ~/.$2.${DATE}"
-			mv ~/.$2 ~/.$2.${DATE}
+			print_msg "INFO: Directory ${HOME}/.${DIR} backup to ${HOME}/.${DIR}.${DATE}"
+			mv "${HOME}/.${DIR}" "${HOME}/.${DIR}.${DATE}"
 		fi
-		print_msg "UPDATE: Create link ~/.$2 to $1/$2"
-		ln -s $1/$2 ~/.$2
+		print_msg "UPDATE: Create link ${HOME}/.${DIR} to ${ROOT_DIR}/${DIR}"
+		ln -s "${ROOT_DIR}/${DIR}" "${HOME}/.${DIR}"
 	fi
-	bash $0 ~/.$2
+	bash "${0}" "${HOME}/.${DIR}"
 	return 0
 }
 
 if test $# -eq 0
 then
-	DOT_PATH=~/.dot
+	DOT_PATH="${HOME}/.dot"
 else
-	DOT_PATH=$1
+	DOT_PATH="${1}"
 fi
-DOT_FILES=$DOT_PATH/.dotfiles
+DOT_FILES="${DOT_PATH}/.dotfiles"
 
-if test -f $DOT_FILES
+if test -f "${DOT_FILES}"
 then
-	for element in `cat $DOT_FILES`
+	while read ELEMENT
 	do
-		if test -d $element
+		if test -d "${ELEMENT}"
 		then
-			dot_dir $DOT_PATH $element
+			dot_dir "${DOT_PATH}" "${ELEMENT}"
 		else
-			dot_file $DOT_PATH $element
+			dot_file "${DOT_PATH}" "${ELEMENT}"
 		fi
-	done
+	done < "${DOT_FILES}"
 fi
